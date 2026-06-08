e9c3df8fbbc10924dfe0426d8a48ea074c7440b9
lindath0905 <linda.thurner@outlook.at>
2026-06-08 16:53:19 +0200

    first setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Dictionary App</header>
+      <Dictionary />
+    </div>
+  );
+}
+
+export default App;
