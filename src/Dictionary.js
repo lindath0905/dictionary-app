3cab7ee07e8b9e9470a2e6a6bb5a32ae97f55809
lindath0905 <linda.thurner@outlook.at>
2026-06-09 14:47:54 +0200

    integrated api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,9 +5,17 @@ import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    let apiURL = "https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}";
+    
+    axios.get(apiURL).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
