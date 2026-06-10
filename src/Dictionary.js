219ad3b3b3d995b249a95641b0dd514521e5e048
lindath0905 <linda.thurner@outlook.at>
2026-06-10 15:25:48 +0200

    quick fix

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState([]);
 
   function handleResponse(response) {
-    console.log(response);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
 
-    let apiURL = "https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}";
-    
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
     axios.get(apiURL).then(handleResponse);
   }
 
@@ -27,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autofocus="on" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
