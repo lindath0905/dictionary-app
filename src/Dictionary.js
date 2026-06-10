32a60c1195282f3607864008168d2c56a3651aed
lindath0905 <linda.thurner@outlook.at>
2026-06-10 15:27:53 +0200

    quick fix

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Results from "Results";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
